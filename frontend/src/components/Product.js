f373f9e9eb0fb2ac1466ebeaf7a416effb73e77e
aviad777 <aviadhaimjan@gmail.com>
2020-12-24 15:40:47 +0200

    cart_sketch

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Rating from './Rating';
+
+export default function Product(props) {
+    const { product } = props;
+    console.log('product is:', product);
+    return (
+        <div key={product._id} className="card">
+            <a href={`/product/${product._id}`}>
+                <img className="medium" src={product.image} alt={product.name} />
+            </a>
+            <div className="card-body">
+                <a href="product.html">
+                    <h2>
+                        {product.name}
+                    </h2>
+                </a>
+                <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
+                <div className="price">${product.price}</div>
+            </div>
+        </div>)
+}
+
+
